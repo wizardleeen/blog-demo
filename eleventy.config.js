c4caae905e943a131d866e7146039161d4608983
wizardleen <wizardleen@gmail.com>
2026-01-02 19:20:11 +0800

    add install.sh

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
     // Tell 11ty to copy the "css" folder to the output folder
     eleventyConfig.addPassthroughCopy("src/css");
-    
+    eleventyConfig.addPassthroughCopy({"src/install.sh": "install.sh"})
     // (Optional) Copy images if you had an images folder
     // eleventyConfig.addPassthroughCopy("images");
   
